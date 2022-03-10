21d0b80767d2f19df40dfa292dcd0a5a0e087a36
Emil Eriksson <asheidan@gmail.com>
2022-03-10 20:25:12 +0100

    Better output format

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ struct Options {
 
 fn main() {
     let options = Options::parse();
-    println!("{:?}", options);
+    eprintln!("{:?}", options);
 
     let date: chrono::NaiveDate = match options.date {
         Some(date_string) => {
@@ -29,14 +29,14 @@ fn main() {
         }
         None => chrono::offset::Local::now().date().naive_local(),
     };
-    //println!("{:?}", date);
+    //eprintln!("{:?}", date);
 
     let month_directory_path = format!(
         "{}/{}",
         options.worklog_dir,
         date.format(&options.directory_pattern[..]).to_string(),
     );
-    //println!("{}", month_directory_path);
+    //eprintln!("{}", month_directory_path);
 
     let interval_pattern = date
         .format(
@@ -45,86 +45,89 @@ fn main() {
         .to_string();
     let interval_regex = Regex::new(&interval_pattern[..]).unwrap();
 
-    let available_files = fs::read_dir(options.worklog_dir).unwrap()
+    let available_paths = fs::read_dir(options.worklog_dir).unwrap()
         .into_iter()
         .chain(fs::read_dir(month_directory_path).unwrap().into_iter())
-        .filter(|entry| match entry {
+        .filter_map(|entry| match entry {
             Ok(file) => {
-                match file.metadata() {
-                    Ok(metadata) => {
-                        let file_name = file.file_name().into_string().unwrap();
-                        metadata.is_file() && !file_name.starts_with(".") && file_name.ends_with(".org")
+                let file_name_maybe = file.file_name().into_string();
+                match file_name_maybe {
+                    Ok(normalized_file_name) => {
+                        match file.metadata() {
+                            Ok(metadata) => {
+                                match metadata.is_file() && !normalized_file_name.starts_with(".") && normalized_file_name.ends_with(".org") {
+                                    true => Some(file.path()),
+                                    false => None,
+                                }
+                            },
+                            _ => None,
+                        }
                     },
-                    _ => false,
+                    _ => None,
                 }
             },
-            _ => false,
-        })
-        .filter_map(|entry| match entry {
-            Ok(entry) => {
-                let file = fs::File::open(entry.path()).unwrap();
-                let reader = io::BufReader::new(file);
-                Some(reader.lines())
-            },
             _ => None,
+        });
+    let timestamps = available_paths
+        .map(|file_path| fs::File::open(file_path))
+        .filter_map(|maybe_file| match maybe_file {
+            Ok(file) => Some(io::BufReader::new(file).lines()),
+            Err(_) => None,
         })
         .flatten()
-        .filter_map(|s| {
-            let line = &s[..];
-            match interval_regex.captures(line) {
-            }
-        });
-
-    for entry in available_files {
-        match entry {
-            Ok(file) => {
-                println!("{:?}", file.path());
-            },
-            Err(e) => {
-                println!("{:?}", e);
-            }
-        }
-    }
-    println!("{}", "--- End of files");
-
-    let stdin = std::io::stdin();
-    let timestamps = stdin
-        .lock()
-        .lines()
-        .filter_map(|s| {
-            let line = &s.unwrap()[..];
-            match interval_regex.captures(line) {
-                Some(captures) => {
-                    let timestamps = [&captures[1], &captures[2]];
-                    let datetimes =
-                        timestamps.map(|ts| {
+        .filter_map(|io_lines| match io_lines {
+            Ok(s) => {
+                let line = &s[..];
+                match interval_regex.captures(line) {
+                    Some(captures) => {
+                        let dates = [&captures[1], &captures[2]].map(|ts|{
                             match chrono::NaiveDateTime::parse_from_str(ts, "%Y-%m-%d %a %H:%M") {
                                 Ok(datetime) => Some(datetime),
                                 Err(err) => {
-                                    println!("{:?}", err.to_string());
+                                    eprintln!("{:?}", err.to_string());
                                     None
                                 }
                             }
                         });
-                    match datetimes {
-                        [Some(start), Some(end)] => {
-                            Some((start.date(), (end - start).num_seconds()))
-                        }
-                        _ => None,
-                    }
+                        Some(dates)
+                    },
+                    _ => None,
                 }
-                None => None,
             }
+            _ => None,
         })
+        .filter_map(|datetimes| {
+            match datetimes {
+                [Some(start), Some(end)] => {
+                    Some((start.date(), (end - start).num_seconds()))
+                }
+                _ => None,
+            }
+        });
+
+    let mapped_timestamps = timestamps
         .fold([0; 31], |mut acc, (date, duration)| {
             acc[(date.day() - 1) as usize] += duration;
             acc
+        })
+        .map(|number| (number as f64 / 900.0).round() / 4.0);
+
+    let mapped_strings = mapped_timestamps
+        .map(|number| match number {
+            f if (-1.0 ..= 0.0).contains(&f) => String::from(""),
+            f => format!("{:.2}", f),
         });
-    for (index, timestamp) in timestamps.iter().enumerate() {
-        println!(
-            "{:?}: {:?}",
+    
+    for (index, duration_string) in mapped_strings.iter().enumerate() {
+        eprintln!(
+            "{:2}:{:>6}",
             index + 1,
-            (*timestamp as f64 / 900.0).round() / 4.0
+            duration_string,
         );
     }
+    eprintln!("T:{:>7.2}", mapped_timestamps.iter().sum::<f64>());
+
+    eprintln!("--------------------------------------------------------------------------------");
+    let row = mapped_strings.join("\t");
+    println!("{}", row);
 }
